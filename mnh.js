ede15a6bcf02c3e9b51ec3cbccc5cb2a1d3ca6b7
Kolapo Oni <onikolapo@gmail.com>
2017-11-02 09:41:13 +0100

    kcnlksc

--- mnh.js
+++ mnh.js
@@ -29,7 +29,7 @@ loader.add(["shaq.png", "shaq-frown.png"]).load(setup);
 
 function setup() {
     if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        heightDivider = 1.55;
+        heightDivider = 1.5;
         scale = 2;
     }
     else if(/iPad/i.test(navigator.userAgent)){
